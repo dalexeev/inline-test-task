9e91f03bb651294bcad8d70749bf7ab9195c34bb
Danil Alexeev <dalexeev12@yandex.ru>
2024-12-03 13:30:35 +0300

    Fix colon style in comment title

--- views/comments.php
+++ views/comments.php
@@ -91,7 +91,8 @@ $output_comment_body = function (string $text) use ($query_words, $words_regex):
 		<span class="comment-author"><?= html($comment['name']) ?></span>
 		<span class="comment-email">&lt;<?= html($comment['email']) ?>&gt;</span>
 		прокомментировал(а) запись
-		<span class="comment-post">"<?= html($comment['postTitle']) ?>":</span>
+		<span class="comment-post">"<?= html($comment['postTitle']) ?>"</span>
+		<span>:</span>
 	</div>
 	<div class="comment-body"><?php $output_comment_body($comment['body']); ?></div>
 </div>
